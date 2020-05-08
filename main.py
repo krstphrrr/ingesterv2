0c8212be3b7e8dfd0bb44679846c2331d359ea4d
Kris! <42949874+krstphrrr@users.noreply.github.com>
2020-05-08 12:49:19 -0600

    modularizing some tables for later; reestablishing geoindicators w/o geo

--- main.py
+++ main.py
@@ -8,59 +8,8 @@ from sqlalchemy import *
 from shapely.geometry import Point
 import re
 
-
-class header:
-    engine = None
-    initial_dataframe = None
-    geo_dataframe = None
-    header_types = {
-        "PrimaryKey":VARCHAR(100),
-        "SpeciesState": VARCHAR(2),
-        "PlotID": TEXT(),
-        "PlotKey": VARCHAR(50),
-        "DBKey": TEXT(),
-        "EcologicalSiteId": VARCHAR(50),
-        "Latitude_NAD83": NUMERIC(),
-        "Longitude_NAD83": NUMERIC(),
-        "State": VARCHAR(2),
-        "County": VARCHAR(50),
-        "DateEstablished": DATE(),
-        "DateLoadedInDb": DATE(),
-        "ProjectName": TEXT(),
-        "source": TEXT(),
-        "LocationType": VARCHAR(20),
-        "DateVisited": DATE(),
-        "Elevation": NUMERIC(),
-        "PercentCoveredByEcoSite": NUMERIC(),
-        'wkb_geometry': Geometry('POINT', srid=4326)
-        }
-    def __init__(self,path):
-
-        """ clearing attributes & setting engine """
-        self.engine = create_engine(os.environ.get('DBSTR'))
-        self.initial_dataframe = None
-        self.geo_dataframe = None
-
-        """ prepping a geodf from path """
-        self.initial_dataframe = pd.read_csv(path)
-        self.gdf = gpd.GeoDataFrame(
-            self.initial_dataframe,
-            crs='epsg:4326',
-            geometry = [
-                Point(xy) for xy in zip(self.initial_dataframe.Longitude_NAD83,
-                self.initial_dataframe.Latitude_NAD83)
-                ]
-            )
-        self.geo_dataframe['wkb_geometry'] = self.geo_dataframe['geometry'].apply(lambda x: WKTElement(x.wkt, srid=4326))
-        self.geo_dataframe.drop('geometry', axis=1, inplace=True)
-
-    def send_to_pg(self):
-
-        self.geo_dataframe.to_sql('dataHeader', self.engine, index=False, dtype=self.header_types)
-
-
-
-i = ingesterv2()
+from models.header import header
+from models.geospecies import geoSpecies
 
 no = ['groups', 'ng_user', 'pages', 'PrimaryKey', 'user_group_link', 'users']
 for item in i.tablenames:
@@ -68,13 +17,13 @@ for item in i.tablenames:
         i.drop_fk(item)
 
 
-gdf.to_sql('dataHeader', engine, index=False, dtype=header_types)
 
 
 
-i.drop_fk('dataGap')
-i._drop_fk('dataHeight')
-i.drop_table('dataHeader_geo')
+i = ingesterv2()
+i.tablenames
+
+i.reestablish_fk
 class ingesterv2:
     # connection properties
     con = None
---
+++ models/header.py
@@ -0,0 +1,58 @@
+import os, os.path, pandas as pd
+from pandas import read_sql_query
+from psycopg2 import connect, sql
+from utils.tools import db, config, geoconfig
+import geopandas as gpd
+from geoalchemy2 import Geometry, WKTElement, WKBElement
+from sqlalchemy import *
+from shapely.geometry import Point
+import re
+
+class header:
+    engine = None
+    initial_dataframe = None
+    geo_dataframe = None
+    header_types = {
+        "PrimaryKey":VARCHAR(100),
+        "SpeciesState": VARCHAR(2),
+        "PlotID": TEXT(),
+        "PlotKey": VARCHAR(50),
+        "DBKey": TEXT(),
+        "EcologicalSiteId": VARCHAR(50),
+        "Latitude_NAD83": NUMERIC(),
+        "Longitude_NAD83": NUMERIC(),
+        "State": VARCHAR(2),
+        "County": VARCHAR(50),
+        "DateEstablished": DATE(),
+        "DateLoadedInDb": DATE(),
+        "ProjectName": TEXT(),
+        "source": TEXT(),
+        "LocationType": VARCHAR(20),
+        "DateVisited": DATE(),
+        "Elevation": NUMERIC(),
+        "PercentCoveredByEcoSite": NUMERIC(),
+        'wkb_geometry': Geometry('POINT', srid=4326)
+        }
+    def __init__(self,path):
+
+        """ clearing attributes & setting engine """
+        self.engine = create_engine(os.environ.get('DBSTR'))
+        self.initial_dataframe = None
+        self.geo_dataframe = None
+
+        """ prepping a geodf from path """
+        self.initial_dataframe = pd.read_csv(path)
+        self.gdf = gpd.GeoDataFrame(
+            self.initial_dataframe,
+            crs='epsg:4326',
+            geometry = [
+                Point(xy) for xy in zip(self.initial_dataframe.Longitude_NAD83,
+                self.initial_dataframe.Latitude_NAD83)
+                ]
+            )
+        self.geo_dataframe['wkb_geometry'] = self.geo_dataframe['geometry'].apply(lambda x: WKTElement(x.wkt, srid=4326))
+        self.geo_dataframe.drop('geometry', axis=1, inplace=True)
+
+    def send_to_pg(self):
+
+        self.geo_dataframe.to_sql('dataHeader', self.engine, index=False, dtype=self.header_types)
