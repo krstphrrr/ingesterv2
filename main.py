68d05b47f74c634cc9e879aebbd59545915acb95
Kris! <42949874+krstphrrr@users.noreply.github.com>
2020-06-03 23:27:50 -0600

    lpi changes

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 import os, os.path, pandas as pd
 from pandas import read_sql_query
 from psycopg2 import connect, sql
+import psycopg2
 from utils.tools import db, config, geoconfig
 import geopandas as gpd
 from geoalchemy2 import Geometry, WKTElement, WKBElement
 from sqlalchemy import *
 from shapely.geometry import Point
 import re
+from io import StringIO
 
-from models.header import header
-from models.geospecies import geoSpecies
+import psycopg2
+from tqdm import tqdm
+
+# from models.header import header
+# from models.geospecies import geoSpecies
+from models.lpi import datalpi
 
 no = ['groups', 'ng_user', 'pages', 'PrimaryKey', 'user_group_link', 'users']
 for item in i.tablenames:
     if item not in no:
         i.drop_fk(item)
 
-
-
-
-
+lpipath = r"C:\Users\kbonefont\Desktop\data\lpi_tall.csv"
+lpi = datalpi(lpipath)
+lpi.checked_df['chckbox'].unique()
+lpi.initial_dataframe.loc["PrimaryKey"]
+lpi.initial_dataframe
+for i in lpi.initial_dataframe.columns:
+    print(i, lpi.initial_dataframe[i].dtype)
+import numpy as np
+lpi.initial_dataframe['chckbox'].apply(lambda x: pd.NA if pd.isnull(x)==True else x).astype("Int64")
+np.nan==lpi.initial_dataframe['chckbox'][12509196]
+pd.NA
+lpi.initial_dataframe["chckbox"].astype("int64")
+lpi.send_to_pg()
 i = ingesterv2()
 i.tablenames
-
-i.reestablish_fk('geoSpecies')
+lpi.initial_dataframe["PrimaryKey"][0]
+i.drop_table('dataLPI')
 class ingesterv2:
     # connection properties
     con = None
@@ -89,13 +104,12 @@ class ingesterv2:
         print(f"Foreign keys on {table} dropped")
     def drop_table(self, table):
         try:
-
             self.cur.execute(
             sql.SQL("DROP TABLE IF EXISTS gisdb.public.{};").format(
-            sql.Identifier(item))
+            sql.Identifier(table))
             )
             self.con.commit()
-            print(item +' dropped')
+            print(table +' dropped')
         except Exception as e:
             print(e)
             self.con = db.str
@@ -122,7 +136,98 @@ class ingesterv2:
 
 
 
+df = pd.DataFrame({'float': [1.0],
+                   'int': [1],
+                   'datetime': [pd.Timestamp('20180310')],
+                   'string': ['foo']})
 
+df.datetime.dtype!=='datetime64[ns]'
+test.DateModified.astype('datetime64[ns]')
+test = lpi.initial_dataframe[:5].copy(deep=True)
+for i in test.columns:
+    if test[i].dtype!=datalpi_dtypes[i]:
+        test[i] = typecast(test,i,datalpi_dtypes[i])
+test["PointNbr"].unique()
+[i for i in test.columns]
+lpi.initial_dataframe["PointLoc"].unique()
+lpi.initial_dataframe["chckbox"].unique()
+lpi.initial_dataframe.iloc[:30,:]
+datalpi_dtypes = {
+        "LineKey" : "object",
+        "RecKey" : "object",
+        "DateModified" : "datetime64[ns]",
+        "FormType" : "object",
+        "FormDate" : "object",
+        "Observer" : "object",
+        "Recorder" : "object",
+        "DataEntry" : "object",
+        "DataErrorChecking" : "object",
+        "Direction" : "object",
+        "Measure" : "float64",
+        "LineLengthAmount" : "float64",
+        "SpacingIntervalAmount" : "float64",
+        "SpacingType" : "object",
+        "HeightOption" : "object",
+        "HeightUOM" : "object",
+        "ShowCheckbox" : "float64",
+        "CheckboxLabel" : "object",
+        "PrimaryKey" : "object",
+        "DBKey" : "object",
+        "PointLoc" : "float64",
+        "PointNbr" : "float64",
+        "ShrubShape" : "object",
+        "layer" : "object",
+        "code" : "object",
+        "chckbox" : "int64",
+        "source" : "object",
+        "STATE" : "object",
+        "SAGEBRUSH_SPP": "object",
+        "PLOTKEY":"object"
+        }
+def typecast(df,field,fieldtype):
+    data = df
+    castfield = data[field].astype(fieldtype)
+    return castfield
+typecast(test, 'chckbox', datalpi_dtypes['chckbox'])
+test.iloc[:,:15]
+test.iloc[:,15:]
+def copy_from(df: pd.DataFrame,
+              table: str,
+              connection: psycopg2.extensions.connection,
+              chunk_size: int = 10000):
+    cursor = connection.cursor()
+    df = df.copy()
+
+    # escaped = {'\\': '\\\\', '\n': r'\n', '\r': r'\r', '\t': r'\t',}
+    # for col in df.columns:
+    #     if df.dtypes[col] == 'object':
+    #
+    #         for v, e in escaped.items():
+    #             df[col] = df[col].str.replace(v, e)
+    try:
+        for i in tqdm(range(0, df.shape[0], chunk_size)):
+            f = StringIO()
+            chunk = df.iloc[i:(i + chunk_size)]
+
+            chunk.to_csv(f, index=False, header=False, sep='\t', na_rep='default', quoting=None)
+            f.seek(0)
+            cursor.copy_from(f, table, columns=[f'"{i}"' for i in df.columns])
+            connection.commit()
+    except psycopg2.Error as e:
+        print(e)
+        connection.rollback()
+    cursor.close()
+escaped = {'\\': '\\\\', '\n': r'\n', '\r': r'\r', '\t': r'\t',}
+testdf = lpi.checked_df.copy()
+for col in testdf.columns:
+    if testdf.dtypes[col] == 'object':
+        for v, e in escaped.items():
+            testdf[col] = testdf["LineKey"].str.replace(v, e)
+lpi.checked_df.iloc[3134:3137,:].to_csv(StringIO(), index=False, header=False, sep='\t', na_rep='\\N', quoting=None)
+[lpi.checked_df.iloc[3136:3137,i] for i in range(0,len(lpi.checked_df.columns))]
+lpi.checked_df.iloc[0,2]
+copy_from(lpi.checked_df, 'gisdb.public."dataLPI"', db.str, 10000)
+db.str
 
 
 
--- models/lpi.py
+++ models/lpi.py
@@ -4,7 +4,7 @@ from sqlalchemy import *
 class datalpi:
     engine = None
     initial_dataframe = None
-    # geo_dataframe = None
+    checked_df = None
     datalpi_types = {
         "LineKey" : VARCHAR(100),
         "RecKey" : VARCHAR(100),
@@ -32,8 +32,43 @@ class datalpi:
         "layer" : TEXT(),
         "code" : TEXT(),
         "chckbox" : INTEGER(),
-        "source" : TEXT()
+        "source" : TEXT(),
+        "STATE" : VARCHAR(50),
+        "SAGEBRUSH_SPP": TEXT(),
+        "PLOTKEY":VARCHAR(100)
         }
+    datalpi_dtypes = {
+            "LineKey" : "object",
+            "RecKey" : "object",
+            "DateModified" : "datetime64[ns]",
+            "FormType" : "object",
+            "FormDate" : "object",
+            "Observer" : "object",
+            "Recorder" : "object",
+            "DataEntry" : "object",
+            "DataErrorChecking" : "object",
+            "Direction" : "object",
+            "Measure" : "float64",
+            "LineLengthAmount" : "float64",
+            "SpacingIntervalAmount" : "float64",
+            "SpacingType" : "object",
+            "HeightOption" : "object",
+            "HeightUOM" : "object",
+            "ShowCheckbox" : "float64",
+            "CheckboxLabel" : "object",
+            "PrimaryKey" : "object",
+            "DBKey" : "object",
+            "PointLoc" : "float64",
+            "PointNbr" : "float64",
+            "ShrubShape" : "object",
+            "layer" : "object",
+            "code" : "object",
+            "chckbox" : "Int64",
+            "source" : "object",
+            "STATE" : "object",
+            "SAGEBRUSH_SPP": "object",
+            "PLOTKEY":"object"
+            }
 
     def __init__(self,path):
 
@@ -43,9 +78,21 @@ class datalpi:
         # self.gap_dataframe = None
 
         """ prepping a geodf from path """
-        self.initial_dataframe = pd.read_csv(path)
+        self.initial_dataframe = pd.read_csv(path, low_memory=False)
+        self.checked_df = self.initial_dataframe.copy()
 
+        """ fieldtype check """
+        for i in self.checked_df.columns:
+            if self.checked_df[i].dtype!=self.datalpi_dtypes[i]:
+                self.checked_df[i] = self.typecast(df=self.checked_df,field=i,fieldtype=self.datalpi_dtypes[i])
+
+    def typecast(self,df,field,fieldtype):
+        data = df
+        castfield = data[field].astype(fieldtype)
+        # if 'chckbox' in field:
+        #     data[field] = data[field].apply()
+        return castfield
 
     def send_to_pg(self):
 
-        self.initial_dataframe.to_sql('dataHeader', self.engine, index=False, dtype=self.datalpi_types)
+        self.initial_dataframe.to_sql('dataLPI', self.engine, index=False, dtype=self.datalpi_types)
