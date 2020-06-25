cd519ac7750fdd3a3d14d2ba247b804b71279d56
Kris! <42949874+krstphrrr@users.noreply.github.com>
2020-06-24 22:45:41 -0600

    csv or pg switch implemented; need calibration flag

--- projects/dima/dima_handler.py
+++ projects/dima/dima_handler.py
@@ -7,40 +7,27 @@ from utils.tools import  config
 from datetime import datetime
 from psycopg2 import sql
 import pandas as pd
-from projects.dima.tables.bsnepk import bsne_pk
-from projects.dima.tables.lpipk import lpi_pk
-from projects.dima.tables.nopk import no_pk
-from projects.dima.tables.gappk import gap_pk
-from projects.dima.tables.sperichpk import sperich_pk
-from projects.dima.tables.plantdenpk import plantden_pk
-from projects.dima.tables.qualpk import qual_pk
 
 from projects.dima.handler import switcher, tableswitch
 from projects.dima.tabletools import fix_fields, new_tablename, table_create,tablecheck
 from projects.tall_tables.talltables_handler import ingesterv2
 
-path = r"C:\Users\kbonefont\Desktop\DIMA 5.4_BlueMountains_2020-01-06.accdb"
-arc = arcno(path)
-arc.actual_list
 
-
-main_translate('tblSites', path)
-[i for i in d.columns]
 def main_translate(tablename,dimapath, debug=None):
 
-    a = ['tblPlots', 'tblLines', 'tblSpecies','tblSpeciesGeneric','tblSites']
+    a = ['tblPlots', 'tblLines', 'tblSpecies','tblSpeciesGeneric','tblSites','tblPlotNotes']
     b = ['tblSoilStabDetail', 'tblSoilStabHeader']
     c = ['tblSoilPits','tblSoilPitHorizons']
     d = ['tblPlantProdDetail', 'tblPlantProdHeader']
     e = ['tblBSNE_Box', 'tblBSNE_Stack','tblBSNE_BoxCollection', 'tblBSNE_TrapCollection']
 
     types={
-        'a':(None, dimapath, tablename),
-        'b':('soilstab',dimapath, None),
-        'c':('soilpits',dimapath, None),
-        'd':('plantprod',dimapath, None),
-        'e':dimapath,
-        'f':('fake', dimapath, tablename)
+        'a': (None, dimapath, tablename),
+        'b': ('soilstab',dimapath, None),
+        'c': ('soilpits',dimapath, None),
+        'd': ('plantprod',dimapath, None),
+        'e': dimapath,
+        'f': ('fake', dimapath, tablename)
     }
     if tablename in a:
         # no_pk branch
@@ -68,25 +55,30 @@ def main_translate(tablename,dimapath, debug=None):
 
     elif tablename in b:
         # no_pk + soilstab branch
+        print('no_pk; soilstab') if debug else None
         df = switcher[tablename](*types['b'])
         return df
 
     elif tablename in c:
         # no_pk + soilpits branch
+        print('no_pk; soilpits') if debug else None
         df = switcher[tablename](*types['c'])
         return df
 
     elif tablename in d:
         # no_pk + plantprod branch
+        print('no_pk; plantprod') if debug else None
         df = switcher[tablename](*types['d'])
         return df
 
     else:
         # lpi_pk, gap_pk, sperich_pk, plantden_pk, bsne_pk branch
         if tablename in e:
+            print('bsne collection') if debug else None
             retdf = switcher[tablename](types['e'])
             return retdf
         else:
+            print('hmmm?') if debug else None
             df = switcher[tablename](types['e'])
             arc = arcno()
             iso = arc.isolateFields(df,tableswitch[tablename],"PrimaryKey").copy()
@@ -96,8 +88,7 @@ def main_translate(tablename,dimapath, debug=None):
             retdf = pd.merge(target_table, iso, how="inner", on=tableswitch[tablename])
             return retdf
 
-
-def pg_send(table,path):
+def pg_send(table,path,csv=None):
     plot = None
     """
     almost done:
@@ -105,28 +96,31 @@ def pg_send(table,path):
     - create Horizontafllux and dust
 
     """
+    d = db('dima')
     df = main_translate(table,path)
+
     df['DateLoadedInDB'] = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    # dbkey add calibration HERE
     df['DBKey']=split(splitext(path)[0])[1].replace(" ","")
+
     if 'ItemType' in df.columns:
         newtablename = new_tablename(df)
         if tablecheck(newtablename):
-            d = db('dima')
             print('llegue aqui 1')
-            ingesterv2.main_ingest(df, newtablename, d.str, 10000)
+            ingesterv2.main_ingest(df, newtablename, d.str, 10000) if csv else df.to_csv(os.path.join(os.path.dirname(path),table.replace('tbl','')+'.csv'))
         else:
-            d = db('dima')
             table_create(df, newtablename)
             print('llegue a 2')
-            ingesterv2.main_ingest(df, newtablename, d.str, 10000)
+            ingesterv2.main_ingest(df, newtablename, d.str, 10000) if csv else df.to_csv(os.path.join(os.path.dirname(path),table.replace('tbl','')+'.csv'))
+
     else:
         if tablecheck(table):
-            d = db('dima')
-            ingesterv2.main_ingest(df, table, d.str, 10000)
+            ingesterv2.main_ingest(df, newtablename, d.str, 10000) if csv else df.to_csv(os.path.join(os.path.dirname(path),table.replace('tbl','')+'.csv'))
+
         else:
-            d = db('dima')
             table_create(df, table)
-            ingesterv2.main_ingest(df, table, d.str, 10000)
+            ingesterv2.main_ingest(df, newtablename, d.str, 10000) if csv else df.to_csv(os.path.join(os.path.dirname(path),table.replace('tbl','')+'.csv'))
+
 
 
             # modcheck+=1
