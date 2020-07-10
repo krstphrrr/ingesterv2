8945e54e56c7c645323c1c22cd2bce536fe97743
Kris! <42949874+krstphrrr@users.noreply.github.com>
2020-07-10 16:53:32 -0600

    doc'd table_preppers.py

--- projects/nri_data/nri_tools/table_preppers.py
+++ projects/nri_data/nri_tools/table_preppers.py
@@ -2,23 +2,81 @@ import pandas as pd
 import numpy as np
 
 
-def concern(df):
+def concern(df:pd.DataFrame):
+    """ changes to concern table.
+
+    Changes implemented:
+        - change all `Y` and `N` across the fields, to 1 and 0.
+
+    -------------------------
+    Args:
+        df (dataframe)
+
+    -------------------------
+    Returns:
+        Altered dataframe.
+
+    """
     for field in df.columns[5:27]:
         df[field] = df[field].apply(lambda x: 1 if x=='Y' else (0 if x=='N' else x))
     return df
 
-def disturbance(df):
+def disturbance(df:pd.DataFrame):
+    """ changes to disturbance table.
+
+    Changes implemented:
+        - change all `Y` and `N` across the fields, to 1 and 0.
+        - dropped tables not in definitions file.
+
+    -------------------------
+    Args:
+        df (dataframe): dataframe to be changed.
+
+    -------------------------
+    Returns:
+        Altered dataframe.
+
+    """
     addins = ['PrimaryKey','FIPSPSUPNT','DBKey']
     for field in df[[col for col in df.columns if col not in addins]].columns[6:]:
         df[field] = df[field].apply(lambda x: 1 if (x=='Y') else (0 if x=='N' else x)).astype('Int64')
-    df.drop(columns=['LARGER_MAMMALS','LIVESTOCK_TANKS_PIPELINES','MACHINERY','FENCES','OIL_FIELD_EQUIPMENT'], inplace=True)
+    # df.drop(columns=['LARGER_MAMMALS','LIVESTOCK_TANKS_PIPELINES','MACHINERY','FENCES','OIL_FIELD_EQUIPMENT'], inplace=True)
     return df
 
-def ecosite(df):
+def ecosite(df:pd.DataFrame):
+    """ changes to ecosite table.
+
+    Changes implemented:
+        - dropped tables not in definitions file.
+
+    -------------------------
+    Args:
+        df (dataframe): dataframe to be changed.
+
+    -------------------------
+    Returns:
+        Altered dataframe.
+
+    """
     df.drop(columns=['SEQNUM','COVERAGE','START_MARK','END_MARK','ECO_SITE_STATE','ECO_SITE_MLRA','ECO_SITE_LRU','ECO_SITE_NAME'], inplace=True)
     return df
 
-def pastureheights(df):
+def pastureheights(df:pd.DataFrame):
+    """ changes to pastureheights table.
+
+    Changes implemented:
+        - changes inches to feet, add wheight, adds unit column, remove
+          problematic strings from some numeric columns.
+
+    -------------------------
+    Args:
+        df (dataframe): dataframe to be changed.
+
+    -------------------------
+    Returns:
+        Altered dataframe.
+
+    """
     height_1 = df[['SURVEY', 'STATE', 'COUNTY', 'PSU', 'POINT', 'TRANSECT', 'DISTANCE','HPLANT','HEIGHT']]
     height_2 = df[['WPLANT', 'WHEIGHT']]
     height_tail = df[['PrimaryKey', 'FIPSPSUPNT', 'DBKey']]
@@ -84,7 +142,23 @@ def pastureheights(df):
     # almost['HPLANT'] = almost['HPLANT'].apply(lambda x: '' if ('None' in x) else x)
     return almost
 
-def soilhorizon(df):
+def soilhorizon(df:pd.DataFrame):
+    """ changes to soilhorizon table.
+
+    Changes implemented:
+        - add `DEPTH_UNIT` field
+        - replaces with numpy nan anything that is not number or letter in the
+          fields `HORIZON_TEXTURE`, `TEXTURE_MODIFIER` and `EFFERVESCENCE_CLASS`
+
+    -------------------------
+    Args:
+        df (dataframe): dataframe to be changed.
+
+    -------------------------
+    Returns:
+        Altered dataframe.
+
+    """
 
     df1 = df.iloc[:,:7]
     df1['DEPTH_UNIT'] = df['DEPTH'].apply(lambda x: 'in' if pd.isnull(x)!=True else x)
@@ -97,7 +171,22 @@ def soilhorizon(df):
 
     return dff
 
-def statenm(df):
+def statenm(df:pd.DataFrame):
+    """ changes to statenm table.
+
+    Changes implemented:
+        - create a new field `PastureRegion` that assigns a region to a set of states.
+        - removes duplicates
+
+    -------------------------
+    Args:
+        df (dataframe): dataframe to be changed.
+
+    -------------------------
+    Returns:
+        Altered dataframe.
+
+    """
 
     midwest = ['IL', 'IN','IA','MI','MN','MO','OH','WI']
     northeast = ['CT', 'DE', 'ME', 'MD', 'MA', 'NH', 'NJ', 'NY', 'PA', 'RI', 'VT', 'WV']
@@ -106,7 +195,7 @@ def statenm(df):
     seast = ['AL','FL','GA','KY', 'MS','NC','SC','TN','VA']
     west = ['AZ', 'CA', 'ID', 'NV', 'NM', 'OR','UT','WA']
 
-    def chooser(state):
+    def chooser(state:str):
         if state in midwest:
             return 'Midwest'
         elif state in northeast:
@@ -123,7 +212,24 @@ def statenm(df):
     df['PastureRegion'] = '0'
     df['PastureRegion'] = statenm['STABBR'].apply(lambda x: chooser(x))
 
-def pintercept(df):
+    return df.drop_duplicates()
+
+def pintercept(df:pd.DataFrame):
+    """ changes to pintercept table.
+
+    Changes implemented:
+        - change all the `None` strings for numpy nan for the fields `HIT1`,
+          `HIT2`, `HIT3`, `HIT4`, `HIT5`, `HIT6` and `BASAL`.
+
+    -------------------------
+    Args:
+        df (dataframe): dataframe to be changed.
+
+    -------------------------
+    Returns:
+        Altered dataframe.
+
+    """
     df['HIT1'] = df['HIT1'].apply(lambda x: np.nan if ('None' in x ) else x)
     df['HIT2'] = df['HIT2'].apply(lambda x: np.nan if ('None' in x ) else x)
     df['HIT3'] = df['HIT3'].apply(lambda x: np.nan if ('None' in x ) else x)
@@ -134,8 +240,25 @@ def pintercept(df):
     return df
 
 
-def practice(df):
+def practice(df:pd.DataFrame):
+    """ changes to practice table.
 
+    Changes implemented:
+        - merged `P528A` with `P528`
+        - merged `N528A` with `N528`
+        - removed `P528A` and `N528A`
+        - changed all `Y` and `N` to 1 and 0, and changed the pandas
+          columns dtype to integer.
+
+    -------------------------
+    Args:
+        df (dataframe): dataframe to be changed.
+
+    -------------------------
+    Returns:
+        Altered dataframe.
+
+    """
     mask = pd.isnull(df.P528A)!=True
     mask2 = pd.isnull(df.N528A)!=True
 
@@ -154,12 +277,23 @@ def practice(df):
 
     return df
 
-def statenm(df):
-    df.drop(columns=['STATENM'], inplace=True)
-    df = df.drop_duplicates()
-    return df
 
-def point(df):
+
+def point(df:pd.DataFrame):
+    """ changes to point table.
+
+    Changes implemented:
+        - removed 'ESD_FITS_SOIL','ACTIVE_CUTTING'
+        
+    -------------------------
+    Args:
+        df (dataframe): dataframe to be changed.
+
+    -------------------------
+    Returns:
+        Altered dataframe.
+
+    """
     # df.drop(columns=['ESD_FITS_SOIL','ACTIVE_CUTTING'],inplace=True)
     # return df
     return df
