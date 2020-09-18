a2320782e71b9512092c77b98da125875f98b1a2
Kris! <42949874+krstphrrr@users.noreply.github.com>
2020-09-18 17:05:50 -0600

    handling online/local .dat or csv's; next timestamp check

--- src/projects/met_data/dathandler.py
+++ src/projects/met_data/dathandler.py
@@ -2,8 +2,10 @@ import pandas as pd
 import numpy as np
 import sys, pandas as pd
 import os, os.path
-import numpy as np
+import csv
 from sqlalchemy import *
+import requests
+from contextlib import closing
 
 # from tqdm import tqdm
 class datReader:
@@ -30,17 +32,42 @@ class datReader:
         6. append created header to headless df
 
         """
-        self.path = None
+        [self.clear(a) for a in dir(self) if not a.startswith('__') and not callable(getattr(self,a))]
         self.arrays = []
-        self.header = None
-        self.df = None
-        # read the first 4 lines
-        with open(path, 'r') as reader:
-            all_lines = reader.readlines()
-            for each_line in all_lines[:4]:
-                split_line = each_line.split(",")
-                self.arrays.append([each_character.replace("\"","") for each_character in split_line])
         self.path = path
+        # read the first 4 lines
+        if (os.path.splitext(self.path)[1]=='.dat') and ('https' not in self.path):
+            """
+            if its a local .dat file
+            """
+            with open(self.path, 'r') as reader:
+                all_lines = reader.readlines()
+                for each_line in all_lines[:4]:
+                    split_line = each_line.split(",")
+                    self.arrays.append([each_character.replace("\"","") for each_character in split_line])
+
+        elif (os.path.splitext(self.path)[1]=='.dat') and ('https' in self.path):
+            """
+            if its an online .dat file
+            """
+            with closing(requests.get(self.path, stream=True)) as r:
+                all_lines =(line.decode('utf-8') for line in r.iter_lines())
+                for index,each_line in enumerate(all_lines):
+                    if index<=3:
+                        split_line = each_line.split(",")
+                        self.arrays.append([each_character.replace("\"","") for each_character in split_line])
+
+        elif (os.path.splitext(self.path)[1]==".csv"):
+            """
+            if its a csv
+            """
+            with closing(requests.get(self.path, stream=True)) as r:
+                f=(line.decode('utf-8') for line in r.iter_lines())
+                reader = csv.reader(f, delimiter=',', quotechar='"')
+                for index, each_line in enumerate(reader):
+                    if index<=3:
+                        self.arrays.append([each_character.replace("\"","") for each_character in each_line])
+
         while len(self.arrays[0])<25:
             temp_space = ''
             self.arrays[0].append(temp_space)
@@ -50,10 +77,16 @@ class datReader:
             if '%' in self.arrays[n]:
                 self.arrays[n]=i.replace('%', 'perc')
 
-
-        self.df = pd.read_table(path, sep=",", skiprows=4, low_memory=False)
+        if os.path.splitext(self.path)[1]=='.dat':
+            self.df = pd.read_table(self.path, sep=",", skiprows=4, low_memory=False)
+        elif os.path.splitext(self.path)[1]==".csv":
+            self.df = pd.read_csv(self.path, skiprows=4, low_memory=False)
 
         self.df.columns = self.arrays
+
+    def clear(self,var):
+        var = [] if isinstance(var,list) else None
+        return var
 
     def getdf(self):
 
